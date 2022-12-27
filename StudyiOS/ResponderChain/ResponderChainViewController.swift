f35470eafea3b475dfb1971b37dce99101441ce0
nylah-j <nylah.j@kakaoent.com>
2022-12-27 17:33:32 +0900

    TouchableResponderView 파일 분리

--- StudyiOS/ResponderChain/ResponderChainViewController.swift
+++ StudyiOS/ResponderChain/ResponderChainViewController.swift
@@ -57,28 +57,3 @@ extension ResponderChainViewController {
     }
 }
 
-class TouchableResponderView: UIView {
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("touches began")
-        touches.forEach { touch in
-            print("altitudeAngle: \(touch.altitudeAngle)")
-            print("force: \(touch.force)")
-            print("majorRadius: \(touch.majorRadius)")
-            print("maximumPossibleForce: \(touch.maximumPossibleForce)")
-            print("tapCount: \(touch.tapCount)")
-            print("majorRadiusTolerance: \(touch.majorRadiusTolerance)")
-        }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("touchesMoved")
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("touchesCancelled")
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("touchesEnded")
-    }
-}
