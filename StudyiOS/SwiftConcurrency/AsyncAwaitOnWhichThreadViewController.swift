84e4af9e4a0772f7a67ae5681e0a900e09106ebf
RoadtoS7 <kshvv975@gmail.com>
2024-08-28 11:55:54 +0900

    불필요한 주석 제거

--- StudyiOS/SwiftConcurrency/AsyncAwaitOnWhichThreadViewController.swift
+++ StudyiOS/SwiftConcurrency/AsyncAwaitOnWhichThreadViewController.swift
@@ -40,16 +40,6 @@ class AsyncAwaitOnWhichThreadViewController: UIViewController {
         let message: String = mainThreadCheckMessage()
         print("$$ ViewController - notAsyncCall - ", message)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
 
 func mainThreadCheckMessage() -> String {
