8d374691f9d5b998f80a189dc396998dbb455d0e
RoadtoS7 <kshvv975@gmail.com>
2024-09-05 22:17:36 +0900

    Add MainActorGlobalVariable & Add 백그라운드 mode on

--- StudyiOS/Swift6/GlobalVariableInConcurrency.swift
+++ StudyiOS/Swift6/GlobalVariableInConcurrency.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+@MainActor var mainActorGlobalVariable  = "enable"
+
+struct MainActorGlobalVariable {
+    @MainActor static var enable: Bool = false
+}
+
+struct MainActorGlobalConstant {
+    static let maximum: Int = 1929
+}
+
+nonisolated(unsafe) var candy = ["melon", "orange", "lemon"]
