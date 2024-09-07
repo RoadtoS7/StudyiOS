feb40593caa8479c7a4d870e1b2c62968953993c
RoadtoS7 <kshvv975@gmail.com>
2024-09-07 13:02:11 +0900

    Add Swift6부터 메서드와 파라미터가 모두 동일한 액터 소속이면, 메서드에서 해당 파라미터를 await 없이 접근할 수 있다.
    Swift6부터 Concurrency Safe 체크에서 False Positive 케이스가 해제되었다.

---
+++ StudyiOS/Swift6/DefaultParameterActorIsolation.swift
@@ -0,0 +1,17 @@
+//
+//  DefaultParameterActorIsolation.swift
+//  StudyiOS
+//
+//  Created by nylah.j on 9/2/24.
+//
+
+import Foundation
+
+@MainActor
+class DefaultParameterType {
+    init() {}
+}
+
+@MainActor
+class DefaultParameterActorIsolation {
+}
