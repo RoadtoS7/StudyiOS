18cce997d297827901a1811daf3df16c8cec4196
RoadtoS7 <kshvv975@gmail.com>
2024-09-07 13:15:30 +0900

    Fix - DiffableCollectionView 로 인해 컴파일 되지 않는 이슈 수정
    CellRegistartion에서는 closure에서 itemIdentifier을 받을 수 있다.
    DataSource에서는 item을 dequeue시에 전달할 수 있다.
    
    추가적으로 궁금한 사항, dataSource의 cellProvider에서 unowend self를 하지 않아도 괜찮은가?

--- StudyiOS/CollectionViewController/DiffableDataSource/DiffableCollectionView.swift
+++ StudyiOS/CollectionViewController/DiffableDataSource/DiffableCollectionView.swift
@@ -14,14 +14,23 @@ final class DiffableCollectionView: UIViewController {
     typealias DataSource = UICollectionViewDiffableDataSource<SectionId, ItemId>
     private let collectionView: UICollectionView =  .init(frame: .zero)
     private var dataSource: DataSource!
+    var items: [Int] = (0...10).map { $0 }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let cellRegistration = UICollectionView.CellRegistration { cell, indexPath, itemIdentifier in
+        typealias CellItem = Int
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, CellItem> { cell, indexPath, itemIdentifier in
+            var contentConfig = cell.defaultContentConfiguration()
+            contentConfig.text = "(row: \(indexPath.row)) (item: \(indexPath.item))  (section: \(indexPath.section)"
+            cell.contentConfiguration = contentConfig
             
         }
+
+        // escpaing closure이다.
+        // strong refrence cycle이 발생할 우려가 있는가?
+        // self가 dataSource를 갖는다. dataSource가 closure를 갖는다.
         dataSource = DataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
-            collectionView.dequeueConfiguredReusableSupplementary(using: cellRegistration, for: indexPath)
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: self.items[itemIdentifier])
         })
         collectionView.dataSource = dataSource
         
